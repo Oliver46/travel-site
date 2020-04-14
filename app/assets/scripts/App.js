4d52785fb573af9f9b1bbd75b037846019291ae3
Oliver <oliver@ableengine.com>
2020-04-14 15:30:54 -0400

    completed lay loading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes' /* download images when user scrolldown */
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll' 
 import StickyHeader from './modules/StickyHeader'
