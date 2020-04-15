2ede2b346fa84cce1a11ff02ea62f912718b5952
Oliver <oliver@ableengine.com>
2020-04-15 16:45:04 -0400

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,25 +1,42 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "Content-Type"
+    }
+   
+    if (event.httpMethod !== "POST") {
+      return callback(null, {
+        statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+      })
+    }
+    
     const secretContent = `
-    <h3>Welcome To the secret area</h3>
-    <p>Here we can tell that the sky is <strong>blue</strong>, and 2 + 2 = 4</p>
-    ` 
-    let body 
-
-    if(event.body){
-        body = JSON.parse(event.body)
-    }else{
-        body = {}
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `
+    
+    let body
+   
+    if (event.body) {
+      body = JSON.parse(event.body)
+    } else {
+      body = {}
     }
-
-    if(body.password == "javascript"){
-        callback(null, {
-            statusCode:200,
-            body: secretContent
-        })
-    }else{
-        callback(null, {
-            statusCode:401,
-        })
+   
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: secretContent
+      })
+    } else {
+      callback(null, {
+        statusCode: 401,
+        headers
+      })
     }
-
-}
+   
+    
+  }
